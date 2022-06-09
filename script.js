7c8557de9a75416108065e7a68aab95d3784796e
Tiana-Tian <Tiana-Tian@users.noreply.github.com>
2022-06-09 22:09:23 +1000

    W4 - Capture user input to the task list

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-//Create array to store tasks
-var taskList = []
+// Setting up variables for our HTML elements using DOM selection
+const form = document.getElementById("taskform");
+const button = document.querySelector("#taskform > button"); // Complex CSS query
+const tasklist = document.getElementById("tasklist");
+const taskInput = document.getElementById("taskInput");
 
-//Define a function for dynamically creating task objects
-function addTask(name, description, startDate, dueDate, estimatedCompletionTime) {
+// Event listener for Button click
+// This could also be form.addEventListener("submit", function() {...} )
+button.addEventListener("click", function(event) {
+  event.preventDefault(); // Not as necessary for button, but needed for form submit
 
-    //create the task object
-    let task = {
-        name, 
-        description, 
-        startDate, 
-        dueDate, 
-        estimatedCompletionTime,
-        taskStarted: false,
-        taskCompleted: false
-    }
+  let task = form.elements.task.value; // could be swapped out for line below
+  //let task = taskInput.value;
 
-    //push task object to taskList array
-    taskList.push(task);
+  let date = (new Date()).toLocaleDateString('en-US') //Convert to short date format
+
+  // Call the addTask() function using
+  addTask(task, date, "26/03/2021", "Low", ["1", "30"], false);
+
+  // Log out the newly populated taskList everytime the button has been pressed
+  console.log(taskList);
+})
+
+// Create an empty array to store our tasks
+var taskList = [];
+
+function addTask(taskDescription, createdDate, dueDate, priorityRating, estimatedTime, completionStatus) {
+  let task = {
+    taskDescription,
+    createdDate,
+    dueDate,
+    priorityRating,
+    estimatedTime,
+    completionStatus
+  };
+
+  // Add the task to our array of tasks
+  taskList.push(task);
+
+  // Separate the DOM manipulation from the object creation logic
+  renderTask(task);
 }
-    //call the addTask function with some test data
-    addTask("Finish Tutorial Exercises", "Use the Canvas summary to finish the exercises", "13/03/2022", "13/03/2022", 30)
-    addTask("Start thinking about the assessment", "...", "20/03/2022", "20/03/2022", 60)
-    
-// Log out the overall array
-console.log(taskList);
+
+
+// Function to display the item on the page
+function renderTask(task) {
+  let item = document.createElement("li");
+  item.innerHTML = "<p>" + task.taskDescription + "</p>";
+
+  tasklist.appendChild(item);
+
+  // Setup delete button DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton); // Adds a delete button to every task
+
+  // Listen for when the 
+  delButton.addEventListener("click", function(event){
+    item.remove(); // Remove the task item from the page when button clicked
+    // Because we used 'let' to define the item, this will always delete the right element
+  })
+  
+  // Clear the value of the input once the task has been added to the page
+  form.reset();
+}
